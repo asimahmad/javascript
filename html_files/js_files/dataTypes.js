10b054b1c3ce690e502638d193957b29bdaff13a
Asim <asim005011997@gmail.com>
2020-08-26 18:34:31 +0530

    Data types in JavaScript

---
+++ html_files/js_files/dataTypes.js
@@ -0,0 +1,35 @@
+console.log("Hello everyone");
+document.write("This is written from java script.");
+var general;
+console.log(general);
+general = 1;
+console.log(typeof(general));
+general = "Asim";
+console.log(typeof(general));
+general = true;
+console.log(typeof(general));
+general = null;
+console.log(typeof(general));
+var space = ["Asim", 1, "Kaush"];
+console.log(space.length);
+console.log(typeof(space));
+var space = new Array("Aakash", "Ankit");
+console.log(space.length);
+console.log(space[1]);
+console.log(typeof(space));
+var number = 5;
+number +=5;
+console.log(number);
+console.log(number+10);
+console.log(number+"20");
+var input = prompt("Enter any number"); // by default prompt's return type is String.
+console.log(input);
+console.log(typeof input);
+// if(input === Number.NaN){
+// location.reload(true);
+// }
+// else{
+//     console.log(parseInt(input)+2);
+// }
+
+alert("Hi there");
---
+++ html_files/js_files/main.js
@@ -0,0 +1,2 @@
+document.getElementById("para").innerHTML = "This is dynamic paragraph."
+console.log("good");
